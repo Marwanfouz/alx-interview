1a2f8fcf6e987dcab283b53f53198e3314bd4695
Marwan Fouz <marwanfouz76@gmail.com>
2024-01-14 18:30:13 +0200

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+  li = [0]
+  lin = 0
+  while (lin < 2):
+    for i, w in enumerate(boxes):
+      if i in li:
+        for j in w:
+          li.append(j)
+    lin += 1
+  for i in range(len(boxes)):
+      if i not in li:
+        return False
+  return True
