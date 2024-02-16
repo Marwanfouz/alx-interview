b2ba62be939d27530a91f9186f397bd49c143b9b
Marwan Fouz <marwanfouz76@gmail.com>
2024-02-16 03:57:25 +0200

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,14 @@ const request = require('request');
 id = process.argv[2]
 films = `https://swapi-api.hbtn.io/api/films/${id}`
 
-request(films,(err, res, body) => {
+request(films, (err, res, body) => {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x],(err, res, body) => {
+  request(actors[x], (err, res, body) => {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);
